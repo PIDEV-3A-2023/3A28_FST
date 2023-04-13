dcddc6dddd9041e496459f66b12e29ab7ef055f5
malieo1 <zahmoul.malek@etudiant-fst.utm.tn>
2023-04-13 22:50:44 +0200

    modif admin

--- src/Controller/GestionProduitController.php
+++ src/Controller/GestionProduitController.php
@@ -4,11 +4,14 @@ namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Produit;
+use App\Form\ModifproduitType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class GestionProduitController extends AbstractController
 {
@@ -34,4 +37,22 @@ class GestionProduitController extends AbstractController
         return $this->redirectToRoute('app_gestion_produit');
     }
 
+    #[Route('/update/{id}', name: 'updateProduit')]
+    public function  updateProduit(ManagerRegistry $doctrine,$id,  Request  $request) : Response
+    { $produit = $doctrine
+        ->getRepository(Produit::class)
+        ->find($id);
+        $form = $this->createForm(ModifproduitType::class, $produit);
+        $form->add('update', SubmitType::class) ;
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        { $em = $doctrine->getManager();
+            $em->flush();
+            return $this->redirectToRoute('app_gestion_produit');
+        }
+        return $this->renderForm("gestion_produit/update.html.twig",
+            ["form"=>$form]) ;
+
+
+    }
 }
